85415c934c40e8723c00f3df123cb81716de85c0
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2025-02-21 20:36:24 +0630

    setup: property detail page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,14 +1,42 @@
+import PropertyDetails from "@/components/PropertyDetails";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import { connectDB } from "@/config/dbConnect";
+import Property from "@/models/PropertyModel";
+import { PropertyType } from "@/types/PropertyTypes";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+
 interface Props {
   params: { id: string };
-  searchParams: { name: string; age: number };
 }
 
-const PropertyDetailPage = ({ params, searchParams }: Props) => {
-  console.log(searchParams);
+const PropertyDetailPage = async ({ params }: Props) => {
+  await connectDB();
+  const property = await Property.findById(params.id).lean<PropertyType>();
+
   return (
-    <div>
-      PropertyDetailPage {params.id} {searchParams.name} {searchParams.age}
-    </div>
+    <>
+      <PropertyHeaderImage image={property?.images[0] as string} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
+        </div>
+      </section>
+
+      {/* property info */}
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property as PropertyType} />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
---
+++ components/PropertyHeaderImage.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+interface Props {
+  image: string;
+}
+
+const PropertyHeaderImage = ({ image }: Props) => {
+  return (
+    <section>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
+          <Image
+            src={`/images/properties/${image}`}
+            alt=""
+            className="object-cover h-[400px] w-full"
+            width={0}
+            height={0}
+            sizes="100vw"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PropertyHeaderImage;
